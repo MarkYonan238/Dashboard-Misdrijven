1bc8e7e37b8f861830c587c4bafe7896f0df7531
MarkYonan238 <92487308+MarkYonan238@users.noreply.github.com>
2021-11-11 10:44:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ st.sidebar.subheader('Gemaakt Door:')
 st.sidebar.write('• Robin Pelders')
 st.sidebar.write('• Maarten Meeuwes')
 st.sidebar.write('• Mark Yonan')
+st.siderbar.write('#')
 
 
 # ## Politie
